8c5ec1483919c9b74f06004e480df66d9c9477ae
SailorStat <77109932+SailorStat@users.noreply.github.com>
2021-04-07 22:45:17 +0300

    Update clickToLike.js

--- Open code/assets/js/clickToLike.js
+++ Open code/assets/js/clickToLike.js
@@ -1,6 +1,6 @@
 function getClickToLike() {
   const assessments = [...document.querySelectorAll('ytd-toggle-button-renderer')]
-  if (!assessments[0].classList.contains('.style-default-active') && !assessments[1].classList.contains('.style-default-active')) {
+  if (!assessments[0].classList.contains('style-default-active') && !assessments[1].classList.contains('style-default-active')) {
     assessments[0].click()
   }
 }
