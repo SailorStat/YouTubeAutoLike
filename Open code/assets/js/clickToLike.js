0b268b7e607b49d59b4330c252c7ae4fb77179df
SailorStat <77109932+SailorStat@users.noreply.github.com>
2021-04-07 22:32:26 +0300

    create YouTubeAutoLike

---
+++ Open code/assets/js/clickToLike.js
@@ -0,0 +1,8 @@
+function getClickToLike() {
+  const assessments = [...document.querySelectorAll('ytd-toggle-button-renderer')]
+  if (!assessments[0].classList.contains('.style-default-active') && !assessments[1].classList.contains('.style-default-active')) {
+    assessments[0].click()
+  }
+}
+
+setInterval(getClickToLike, 3000)
